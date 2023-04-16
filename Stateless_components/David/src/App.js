6c3a8ce37ec44b27eb44e29df1dccae9ab33a11c
David-Nemecek4227 <115412671+David-Nemecek4227@users.noreply.github.com>
2023-04-16 22:24:42 +0200

    Create App.js

---
+++ Stateless_components/David/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GroupMemberTable from './components/GroupMemberTable';
+
+function App() {
+  return (
+    <div className="App">
+      <GroupMemberTable />
+    </div>
+  );
+}
+
+export default App;
